5aa98a355530c03eebd607a0db82b7df23c2090e
Nicolas Johnson <design@colorik.com>
2019-07-24 23:32:06 -0400

    MAJ WP - 5.2.2

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'colorikwp');
+define('DB_NAME', 'blank_theme');
 
 /** MySQL database username */
 define('DB_USER', 'root');
